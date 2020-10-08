b2c9703cc94d9ccb7e56c59c4d5825a080e9cee5
channaveerayya <channucs0009@gmail.com>
2020-10-08 18:50:56 +0530

    Prepare For Deployment

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const { body } = require('express-validator');
+const express = require("express")
+const connectDB = require("./config/db")
+const { body } = require("express-validator")
+const path = require("path")
 
-const app = express();
-
+const app = express()
 
 // connect Database
 
-connectDB();
+connectDB()
 
 //Init Middleware
-app.use(express.json({ extended: false }));
-
-
-app.get('/', (req, res) => res.send('API RUNNING'));
+app.use(express.json({ extended: false }))
 
 //define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/post'));
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/posts", require("./routes/api/post"))
 
-const PORT = process.env.PORT || 5000;
+//Serve static assets in production
 
-app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`))
